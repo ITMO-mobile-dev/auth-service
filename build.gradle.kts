a075f97a8789a0a3e85ee30e1f595d93aefe3130
mariks1 <marat-khasanshin@list.ru>
2025-03-16 19:54:31 +0300

    changed gradle + docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
-val kotlin_version: String by project
-val logback_version: String by project
-val postgres_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.1.10"
-    id("io.ktor.plugin") version "3.1.1"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
 }
 
-group = "auth"
+group = "ru.ifmo"
 version = "0.0.1"
 
 application {
@@ -18,38 +15,21 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
-kotlin {
-    jvmToolchain(17)
-}
-
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    // Ktor
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("io.ktor:ktor-server-auth")
-    implementation("io.ktor:ktor-server-auth-jwt")
-
-    // Serialization
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.ktor:ktor-server-content-negotiation")
-
-    // Config
-    implementation("io.ktor:ktor-server-config-yaml")
-
-    // PostgreSQL JDBC-драйвер
-    implementation("org.postgresql:postgresql:$postgres_version")
-
-    // Logback
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-
-    implementation("at.favre.lib:bcrypt:0.10.2")
-
-    // Тесты
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    implementation(libs.postgresql)
+    implementation(libs.bcrypt)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
 }
